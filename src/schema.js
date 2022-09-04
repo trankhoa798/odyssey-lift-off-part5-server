594d735d092e6cba79d0bc0ef7e739dea3d1fb57
trankhoa <trankhoa798@gmal.com>
2022-09-04 16:32:45 +0700

    feat: remove deprecate field: length

--- src/schema.js
+++ src/schema.js
@@ -35,8 +35,6 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in seconds"
-    length: Int @deprecated(reason: "Use durationInSeconds instead")
     "The track's full duration, in seconds"
     durationInSeconds: Int
     "The number of modules this track contains"
@@ -64,8 +62,6 @@ const typeDefs = gql`
     id: ID!
     "The module's title"
     title: String!
-    "The module's length in seconds"
-    length: Int @deprecated(reason: "Use durationInSeconds instead")
     "The module's video duration, in seconds"
     durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
